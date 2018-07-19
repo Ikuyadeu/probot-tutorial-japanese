8d611f513c193eadb2e7448398344958531acdba
YukiUeda <ikuyadeu@yahoo.co.jp>
2018-07-19 13:26:23 +0900

    add new code examples

--- my-first-app/src/index.ts
+++ my-first-app/src/index.ts
@@ -1,12 +1,16 @@
-import { Application } from 'probot'
+import { Application } from "probot";
 
 export = (app: Application) => {
-  // Your code here
-  app.log('Yay, the app was loaded!')
+  app.log("Yay, the app was loaded!");
+  app.on("issues.opened", async (context) => {
+    // `context` extracts information from the event, which can be passed to
+    // GitHub API calls. This will return:
+    //   {owner: 'yourname', repo: 'yourrepo', number: 123, body: 'Hello World!}
+    context.log(context.payload);
+    const params = context.issue({body: "Hello World!"});
 
-  // For more information on building apps:
-  // https://probot.github.io/docs/
+    return context.github.issues.createComment(params).then(() => { return; });
+    // return context.github.issues.createComment(context.issue({body: 'Hello World!'});
 
-  // To get your app running against GitHub, see:
-  // https://probot.github.io/docs/development/
-}
+  });
+};
