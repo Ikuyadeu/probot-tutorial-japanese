375f12f31b051020a6930624ad2ab337a066d9b0
YukiUeda <ikuyadeu@yahoo.co.jp>
2018-07-20 15:35:33 +0900

    fix index.ts for auto comlate

--- my-first-app/src/index.ts
+++ my-first-app/src/index.ts
@@ -1,15 +1,15 @@
-import { Application } from "probot";
+import { Application, Context } from "probot";
 
 export = (app: Application) => {
   app.log("Yay, the app was loaded!");
-  app.on("issues.opened", async (context) => {
+  app.on("issues.opened", async (context: Context) => {
     // `context` extracts information from the event, which can be passed to
     // GitHub API calls. This will return:
     //   {owner: 'yourname', repo: 'yourrepo', number: 123, body: 'Hello World!}
     context.log(context.payload);
     const params = context.issue({body: "Hello World!"});
 
-    return context.github.issues.createComment(params).then(() => { return; });
+    context.github.issues.createComment(params);
     // return context.github.issues.createComment(context.issue({body: 'Hello World!'});
 
   });
