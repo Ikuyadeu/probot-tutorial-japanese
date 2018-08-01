e34a10862b7dcd69d2dc5cadc9c5ed414b2483b9
YukiUeda <ikuyadeu@yahoo.co.jp>
2018-08-01 18:33:00 +0900

    fix Hello World source

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ Probotの日本語で開発するためのチュートリアルリポジトリ
 チュートリアル本体は<https://github.com/Ikuyadeu/probot-tutorial-japanese/wiki>
 
 リポジトリ内にはサンプルコードを格納予定
+
+* my-first-app: 投稿したIssueにHello Worldを返信する
--- my-first-app/src/index.ts
+++ my-first-app/src/index.ts
@@ -3,14 +3,14 @@ import { Application, Context } from "probot";
 export = (app: Application) => {
   app.log("Yay, the app was loaded!");
   app.on("issues.opened", async (context: Context) => {
-    // `context` extracts information from the event, which can be passed to
-    // GitHub API calls. This will return:
+    // `context`はGitHubAPIからイベントの情報を抽出します
+    // GitHub API calls.からは以下のような情報が得られます:
     //   {owner: 'yourname', repo: 'yourrepo', number: 123, body: 'Hello World!}
     context.log(context.payload);
     const params = context.issue({body: "Hello World!"});
 
+    // issueにコメントを投稿
     context.github.issues.createComment(params);
-    // return context.github.issues.createComment(context.issue({body: 'Hello World!'});
 
   });
 };
